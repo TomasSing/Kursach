73ec8fddfb1c7a765878ae39066716053ae94358
TomasSing <95038773+TomasSing@users.noreply.github.com>
2021-11-26 10:57:56 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,40 @@ btnBurger.addEventListener("click", () => {
   document.body.classList.toggle("_block");
   html.classList.toggle("_block");
 });
+
+const menuLinks = document.querySelectorAll(".menu-links[data-goto]");
+
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", OnMenuLinkClick);
+
+    function OnMenuLinkClick(e) {
+      const menuLink = e.target;
+      console.log(menuLink)
+      if (
+        menuLink.dataset.goto &&
+        document.querySelector(menuLink.dataset.goto)
+      ) {
+        console.log("rabotai")
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue =
+          gotoBlock.getBoundingClientRect().top +
+          pageYOffset -
+          document.querySelector("header").offsetHeight;
+
+        if (btnBurger.classList.contains("_active")) {
+          menuBar.classList.remove("_active");
+          btnBurger.classList.remove("_active");
+          html.classList.remove("_block");
+          document.body.classList.remove("_block");
+        }
+
+        window.scrollTo({
+          top: gotoBlockValue,
+          behavior: "smooth",
+        });
+        e.preventDefault();
+      }
+    }
+  });
+}
